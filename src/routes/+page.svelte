12218789c196ca2e314c7afd51e9bf515541bdc8
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-10-13 19:31:55 -0500

    hello there

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,28 +29,28 @@
               <span class="card-title">underwind.solutions</span>
             </div>
             <div class="card-content">
-                Hello,
-                <br />
-                thank you for taking the time in reviewing us for your consideration.
-                <br />
-                <br />
-                Websites are like sail boats. They require a main haul or backend API. They require a main mast or frontend interface. When put together and under the right wind-conditions/user-interations your boat/project can successfully navigate to it's destination.
-                <br />
-                <blockquote cite="">
-                  <a href="https://underwind.solutions">https://underwind.solutions</a>
-                </blockquote>
-                Say hello to underwind.solutions: where your project is not just another website; because we treat your project as if it were a high end yacht. We're not as expensive as you might think though, as we only charge per request. So no wind-under-you-sails/user-interactions then no movement/charge.
-                <br />
-                <br />
-                Therefore, the better user experience we create, the more user-interactions your website will get, thus the more profit we can both make. 
-                <br />
-                <br />
-                A wind wind situation :)
-                <br />
-                <br />
-                kind regards,
-                <br />
-                underwind // subvind
+              Hello There!
+              <br />
+              Thank you for taking the time in reviewing us for your consideration.
+              <br />
+              <br />
+              Websites are like sail boats. They require a main haul or backend API. They require a main mast or frontend interface. When put together and under the right wind-conditions/user-interations your boat/project can successfully navigate to it's destination.
+              <br />
+              <blockquote cite="">
+                <a href="https://underwind.solutions">https://underwind.solutions</a>
+              </blockquote>
+              Say hello to underwind.solutions: where your project is not just another website; because we treat your project as if it were a high end yacht. We're not as expensive as you might think though, as we only charge per request. So no wind-under-you-sails/user-interactions then no movement/charge.
+              <br />
+              <br />
+              Therefore, the better user experience we create, the more user-interactions your website will get, thus the more profit we can both make. 
+              <br />
+              <br />
+              A wind wind situation :)
+              <br />
+              <br />
+              kind regards,
+              <br />
+              underwind // subvind
             </div>
             <div class="card-action">
               <a href="https://subvind.com" class="btn black">portfolio</a>
