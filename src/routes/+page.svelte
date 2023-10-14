6001fa1b23273baf418a6414fc0fb2fccc6c1400
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-10-13 20:19:34 -0500

    social

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
+<script lang="ts">
+  import { onMount } from "svelte";
 
+  onMount(() => {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {
+      alignment: 'right'
+    });
+  })
+</script>
 
 <svelte:head>
-    <title>underwind.solutions</title> 
-    <meta name="description" content="Websites are like sail boats. They require a main haul or backend API. They require a main mast or frontend interface. When put together and under the right wind-conditions/user-interations your boat/project can successfully navigate to it's destination." />
+  <title>underwind.solutions</title> 
+  <meta name="description" content="Websites are like sail boats. They require a main haul or backend API. They require a main mast or frontend interface. When put together and under the right wind-conditions/user-interations your boat/project can successfully navigate to it's destination." />
 </svelte:head>
 
 <div class="canvas hide-on-med-and-down">
@@ -28,6 +37,15 @@
               <img src="sailboat.jpg">
               <span class="card-title">underwind.solutions</span>
             </div>
+            <a class="dropdown-trigger btn-floating btn-large waves-effect waves-light black right" style="margin: -2em 2em 0 0;" href='#' data-target='dropdown1'><i class="material-icons">share</i></a>
+
+            <!-- Dropdown Structure -->
+            <ul id='dropdown1' class='dropdown-content' style="width: 200px;">
+              <li><a href="https://www.youtube.com/channel/UCwcibF78LrLQBpfvlApvgKg" target="_blank">YouTube</a></li>
+              <li><a href="https://twitter.com/BurandtTravis" target="_blank">Twitter</a></li>
+              <li><a href="https://github.com/subvind" target="_blank">GitHub</a></li>
+            </ul>
+
             <div class="card-content">
               Hello There!
               <br />
